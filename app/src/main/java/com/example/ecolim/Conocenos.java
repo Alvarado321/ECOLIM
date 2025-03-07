04fde4a66e36ad8bc580bd128c8c4626e1c63a3e
Ronal Alvarado <alvarado_aries_20@hotmail.com>
2025-03-07 08:25:16 -0500

    Feat: Conocenos!

--- app/src/main/java/com/example/ecolim/Conocenos.java
+++ app/src/main/java/com/example/ecolim/Conocenos.java
@@ -1,5 +1,6 @@
 package com.example.ecolim;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,17 +9,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Conocenos extends AppCompatActivity {
+import com.example.ecolim.menu.BaseActivity;
+
+public class Conocenos extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_conocenos);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        configurarToolbar("Conocenos", true);
     }
 }
